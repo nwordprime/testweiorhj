d465762242aaaa2b9c1ded0aed545ebdaf7cf254
nwordprime <sadboltene@gmail.com>
2025-08-01 18:45:55 +0200

    Mettre à jour ReactBitsShowcase avec tous les composants react-bits

--- src/components/ReactBitsShowcase.tsx
+++ src/components/ReactBitsShowcase.tsx
@@ -1,3 +1,1 @@
-{"code":"rate-limited","m\essage":"You have hit the rate limit. Please upgrade to k':st'O gi't:s.# #otcep r isomieg:" alse
-
-#zpRehe=sle # eing,
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to continue using the service.","details":"Rate limit exceeded"}
